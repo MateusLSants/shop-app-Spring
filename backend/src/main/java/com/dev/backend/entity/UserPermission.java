63f437e635b211a309455e05f4b2456ddc8b4b35
Mateus <mateus@pop-os.localdomain>
2022-12-28 12:33:04 -0300

    feat: Crud User Permissions

--- backend/src/main/java/com/dev/backend/entity/UserPermission.java
+++ backend/src/main/java/com/dev/backend/entity/UserPermission.java
@@ -2,25 +2,37 @@ package com.dev.backend.entity;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.Data;
 
 @Entity
-@Table(name = "permission")
+@Table(name = "userPermission")
 @Data
-public class Permission {
+public class UserPermission {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
-    
+
+    @ManyToOne
+    @JoinColumn(name = "UserID")
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "PermissionID")
+    private Permission permission;
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date dateCreated;
     @Temporal(TemporalType.TIMESTAMP)
